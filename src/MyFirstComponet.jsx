3ecc622fc73d3d4e5e64e1fed3667603bbebadc9
Your Name <you@example.com>
2025-11-19 17:08:58 +0200

    c

--- src/MyFirstComponet.jsx
+++ src/MyFirstComponet.jsx
@@ -5,6 +5,7 @@ export default function MyFirstComponent() {
       <h2 className="text-3xl text-blue-500 mt-5">
         This is my first component
       </h2>
+      <h4>This is my first component</h4>
 
       <button className="bg-red-600 py-2 px-4 mt-5">Click</button>
     </div>
