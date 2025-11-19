6d9ac9ca09a3d7c8dff1a0255b39cadabfa4b5d2
Your Name <you@example.com>
2025-11-19 17:00:43 +0200

    react

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import MyFirstComponent from "./MyFirstComponet.jsx"
+function App() {
+  
+
+  return (
+  
+    <>
+      <div>
+        <MyFirstComponent />
+        <h1 className='text-5xl font-bold underline '>
+          hellqo world
+        </h1>
+      </div>
+      <MyFirstComponent />
+    </>
+  )
+}
+
+
+
+export default App
+
---
+++ src/MyFirstComponet.jsx
@@ -0,0 +1,13 @@
+export default function MyFirstComponent() {
+  return (
+    <div className="mt-7">
+      <h1 className="text-7xl text-green-500">Hello World</h1>
+      <h2 className="text-3xl text-blue-500 mt-5">
+        This is my first component
+      </h2>
+
+      <button className="bg-red-600 py-2 px-4 mt-5">Click</button>
+    </div>
+  );
+}
+
