b9f9118ae37a6be504dc4e5cf114da6fad9f38c3
Your Name <you@example.com>
2025-11-20 16:59:08 +0200

    b

--- src/btndev.jsx
+++ src/btndev.jsx
@@ -4,7 +4,9 @@ import TagBtn from './tagBtn.jsx';
 
 export default function BtnDev() {
   return (
-    <div>
+    <div className='flex flex-col gap-1'>
+        <TagBtn />
+        <TagBtn />
         <TagBtn />
     </div>
   );
--- src/tagBtn.jsx
+++ src/tagBtn.jsx
@@ -1,12 +1,12 @@
 export default function BtnTag() {
   return (
-    <div>
+    <div className="flex gap-1">
         <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
             Tag
         </button>
         <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Tag</button>
         <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" >Tag</button>
-        
+
     </div>
   );
 }
